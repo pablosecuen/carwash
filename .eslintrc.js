0f56d179bc4dac97a2cf4e6a137e1e190e8b23bb
Franco de Desarrollo <francoegi01@gmail.com>
2024-03-31 14:55:54 -0300

    feat: create next app and add prettier and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint-config-love',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '.eslintrc.js',
+    'next.config.mjs',
+    'postcss.config.js'
+  ],
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl font-bold'>Welcome to Carwash</h1>
+      <p className='text-lg text-center'>
+        Carwash is a car wash service provider.
+      </p>
+    </main>
+  )
+}
