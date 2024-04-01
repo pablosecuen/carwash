22968759462a9c2a23b211cbbcaca154135310f0
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-01 16:49:57 -0300

    feat: persist customer on create

--- src/actions/create-customer.ts
+++ src/actions/create-customer.ts
@@ -1,9 +1,17 @@
+'use server'
+import { customerRepository } from '@/db/repositories/customer'
+
 export async function createCustomerAction(formData: FormData) {
-  'use server'
+  // TODO: Validate form data
   const data = {
     name: formData.get('name') as string,
     email: formData.get('email') as string,
     phone: formData.get('phone') as string
   }
-  console.log(data)
+  try {
+    const customer = await customerRepository.create(data)
+    console.log('Customer created:', customer)
+  } catch (error) {
+    console.error('Error creating customer:', error)
+  }
 }
