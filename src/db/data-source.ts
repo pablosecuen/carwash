5a07a3d41f8d0f9ab0826242ecd994cfcc918dd6
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-01 16:47:25 -0300

    feat: add typeorm, config and data source

---
+++ src/db/data-source.ts
@@ -0,0 +1,14 @@
+import { dbHost, dbName, dbPass, dbPort, dbUser } from '@/utils/config'
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: dbHost,
+  port: Number(dbPort),
+  username: dbUser,
+  password: dbPass,
+  database: dbName,
+  synchronize: true,
+  logging: false
+})
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,10 @@
-export const { USER_PASS: userPass, EDITOR_PASS: editorPass, ADMIN_PASS: adminPass } = process.env
+export const {
+  USER_PASS: userPass,
+  EDITOR_PASS: editorPass,
+  ADMIN_PASS: adminPass,
+  DB_HOST: dbHost,
+  DB_PORT: dbPort,
+  DB_USER: dbUser,
+  DB_PASS: dbPass,
+  DB_NAME: dbName
+} = process.env
