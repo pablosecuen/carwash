0a61fe0f88ca0f578f100e5f7b7af932804401b4
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-02 08:58:34 -0300

    home login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,30 +14,42 @@ import { SHOW_ROLES } from '@/utils/constants'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl font-bold'>Welcome to Carwash</h1>
-      <p className='text-lg text-center'>Carwash is a car wash service provider.</p>
-      <form action={loginForm} className='space-y-2'>
-        <Select name='role'>
-          <SelectTrigger>
-            <SelectValue placeholder='Selecciona un rol' />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {Object.entries(SHOW_ROLES).map(([role, name]) => (
-                <SelectItem key={role} value={role}>
-                  {name}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <div className='flex w-full max-w-sm items-center space-x-2'>
-          <Input type='password' id='password' name='password' placeholder='Contraseña' />
+    <main className='flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        {/* <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          /> */}
+        <h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          Bienvenido a Carwash
+        </h1>
+        <p className='text-lg mt-5 text-center'>Proveedor de servicios de lavado de coches.</p>
+      </div>
 
-          <Button type='submit'>Ingresar</Button>
-        </div>
-      </form>
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <form action={loginForm} className='space-y-2'>
+          <Select name='role'>
+            <SelectTrigger>
+              <SelectValue placeholder='Selecciona un rol' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {Object.entries(SHOW_ROLES).map(([role, name]) => (
+                  <SelectItem key={role} value={role}>
+                    {name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <div className='flex w-full max-w-sm items-center space-x-2'>
+            <Input type='password' id='password' name='password' placeholder='Contraseña' />
+
+            <Button type='submit'>Ingresar</Button>
+          </div>
+        </form>
+      </div>
     </main>
   )
 }
