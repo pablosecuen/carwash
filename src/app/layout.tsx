4aef288fb3bae81af1614f5258f4df5f3d58740a
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-02 09:25:53 -0300

    add sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/providers/theme/theme-provider'
 import { Providers } from '@/providers/Providers'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Toaster />
       </body>
     </html>
   )
