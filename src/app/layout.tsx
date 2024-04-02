e00fdbcff2bfb0f9a92301394f0ccc3298fc67e3
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-02 09:12:24 -0300

    add next themes dark light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/providers/theme/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,14 +10,23 @@ export const metadata: Metadata = {
   description: 'Carwash is a car wash service provider.'
 }
 
-export default function RootLayout ({
+export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { loginForm } from '@/actions/login-form'
+import { ToggleTheme } from '@/components/theme/ToggleTheme'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import {
@@ -15,16 +16,21 @@ import { SHOW_ROLES } from '@/utils/constants'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm flex items-center justify-end'>
+        <ToggleTheme />
+      </div>
       <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
         {/* <img
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
           /> */}
-        <h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+        <h1 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight '>
           Bienvenido a Carwash
         </h1>
-        <p className='text-lg mt-5 text-center'>Proveedor de servicios de lavado de coches.</p>
+        <p className='text-lg mt-5 text-center opacity-80'>
+          Proveedor de servicios de lavado de coches.
+        </p>
       </div>
 
       <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
