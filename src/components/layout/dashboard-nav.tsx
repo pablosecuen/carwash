7255643795bd09e211cb0f7b64d09493df49c957
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-03 09:29:10 -0300

    feat: nuevos componentes y archivo barril

---
+++ src/components/layout/dashboard-nav.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/lib/utils'
+import { NavItem } from '@/utils/types'
+import { Dispatch, SetStateAction } from 'react'
+
+interface DashboardNavProps {
+  items: NavItem[]
+  setOpen?: Dispatch<SetStateAction<boolean>>
+}
+
+export function DashboardNav({ items, setOpen }: DashboardNavProps) {
+  const path = usePathname()
+
+  if (!items?.length) {
+    return null
+  }
+
+  return (
+    <nav className='grid items-start gap-2 py-5'>
+      {items.map((item, index) => {
+        return (
+          item.href && (
+            <Link
+              key={index}
+              href={item.disabled ? '/' : item.href}
+              onClick={() => {
+                if (setOpen) setOpen(false)
+              }}
+            >
+              <span
+                className={cn(
+                  'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
+                  path === item.href ? 'bg-accent' : 'transparent',
+                  item.disabled && 'cursor-not-allowed opacity-80'
+                )}
+              >
+                {/* TODO: Agregar icono */}
+
+                <span>{item.title}</span>
+              </span>
+            </Link>
+          )
+        )
+      })}
+    </nav>
+  )
+}
---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/lib/utils'
+import { Link } from 'lucide-react'
+import { ToggleTheme } from '../theme/ToggleTheme'
+import { MobileSidebar } from '@/components/layout'
+
+export const Header = () => {
+  return (
+    <div className='fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20'>
+      <nav className='h-14 flex items-center justify-between px-4'>
+        <div className='hidden lg:block'>
+          {/* TODO: poner logo de la empresa */}
+          <img
+            className=' h-10 w-auto'
+            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+            alt='Your Company'
+          />
+        </div>
+        <div className={cn('block lg:!hidden')}>
+          <MobileSidebar />
+        </div>
+
+        <div className='flex items-center gap-2'>
+          {/* <UserNav /> */}
+          <ToggleTheme />
+        </div>
+      </nav>
+    </div>
+  )
+}
