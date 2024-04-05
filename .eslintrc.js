f86b5807d9f227749a71ecf5f1eeafdd1cd4bb96
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-04 21:21:03 -0300

    style: turn off server actions error eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/space-before-function-paren': 'off'
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 }
