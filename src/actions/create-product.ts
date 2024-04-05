4e9d4f55706f1fc3d1c37023284bfcfa96ad4a05
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-04 22:12:17 -0300

    feat: persist product on create

--- src/actions/create-product.ts
+++ src/actions/create-product.ts
@@ -1,11 +1,23 @@
 'use server'
 
+import { productRepository } from '@/db/repositories/product'
+
 export async function createProductAction(formData: FormData) {
+  // TODO: validate
   const data = Object.fromEntries(formData) as {
     name: string
     description: string
     cashPrice: string
     cardPrice: string
   }
-  console.log(data)
+  try {
+    const product = await productRepository.create({
+      ...data,
+      cashPrice: Number(data.cashPrice),
+      cardPrice: Number(data.cardPrice)
+    })
+    console.log({ product })
+  } catch (error) {
+    console.error(error)
+  }
 }
