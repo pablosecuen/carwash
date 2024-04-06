eae7d91e743b0b3db6362967beb86390f54696c3
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-05 21:06:13 -0300

    feat: card component y service summary estilado

--- src/app/(dashboard)/service/ui/summary-service.tsx
+++ src/app/(dashboard)/service/ui/summary-service.tsx
@@ -1,37 +1,43 @@
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { ServiceCheckbox } from './service-checkbox'
 import { Button } from '@/components/ui/button'
 
 export const SummaryService = () => {
   return (
-    <div className='p-5 border rounded-lg m-5 mb-0 flex flex-col items-start justify-between'>
-      <h3>Resumen del servicio</h3>
-      <div>
-        <span>Fecha: </span> <span>12/12/2022</span>
-      </div>
-      <div>
-        <span>Cliente: </span> <span>Imanol Dominguez</span>
-      </div>
-      <div>
-        <span>Vehiculo: </span> <span>Ford Fiesta </span>
-        <span>Servicio: </span> <span>Lavado completo</span>
-      </div>
-      <div>
-        <span>Precio: </span> <span>$500</span>
-      </div>
-      <div>
-        <span>Estado: </span> <span>Pendiente</span>
-      </div>
-      <div>
-        <p>Metodo de pago:</p>
-        <div className='flex items-center gap-x-2 mt-2'>
-          <ServiceCheckbox id='efectivo' label='Efectivo' />
-          <ServiceCheckbox id='tarjeta' label='Tarjeta' />
+    <Card>
+      <CardHeader>
+        <h3>Resumen del servicio</h3>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <span>Fecha: </span> <span>12/12/2022</span>
         </div>
-      </div>
-
-      <div className='flex items-center justify-end w-full'>
-        <Button size={'sm'}>Generar ticket</Button>
-      </div>
-    </div>
+        <div>
+          <span>Cliente: </span> <span>Imanol Dominguez</span>
+        </div>
+        <div>
+          <span>Vehiculo: </span> <span>Ford Fiesta </span>
+          <span>Servicio: </span> <span>Lavado completo</span>
+        </div>
+        <div>
+          <span>Precio: </span> <span>$500</span>
+        </div>
+        <div>
+          <span>Estado: </span> <span>Pendiente</span>
+        </div>
+        <div>
+          <p>Metodo de pago:</p>
+          <div className='mt-2 flex items-center gap-x-2'>
+            <ServiceCheckbox id='efectivo' label='Efectivo' />
+            <ServiceCheckbox id='tarjeta' label='Tarjeta' />
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className='flex w-full items-center justify-end'>
+          <Button>Generar ticket</Button>
+        </div>
+      </CardFooter>
+    </Card>
   )
 }
