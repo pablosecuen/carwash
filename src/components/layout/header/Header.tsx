4ddb8025ad4a015bd6b808d024c5bb7fc0c234c5
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-05 21:05:50 -0300

    feat: estructura estilado

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,8 +4,8 @@ import { MobileSidebar } from '@/components/layout'
 
 export const Header = () => {
   return (
-    <div className='fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20'>
-      <nav className='h-14 flex items-center justify-between px-4'>
+    <div className='supports-backdrop-blur:bg-background/60 container fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur'>
+      <nav className='flex h-14 items-center justify-between px-4'>
         <div className='hidden lg:block'>
           {/* TODO: poner logo de la empresa */}
           <img
