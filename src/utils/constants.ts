3cba1316b1d66e31146821c2763dcb2bb0532657
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-07 12:13:14 -0300

    fix: move users to diferent file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,31 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { adminPass, editorPass, userPass } from './config'
 import { Roles, VehicleType } from './types'
-
-if (userPass == null || editorPass == null || adminPass == null) {
-  throw new Error('Missing user, editor or admin password. Check .env file.')
-}
-
-export const USERS: Record<
-  Roles,
-  {
-    role: Roles
-    pass: string
-  }
-> = {
-  [Roles.USER]: {
-    role: Roles.USER,
-    pass: userPass
-  },
-  [Roles.EDITOR]: {
-    role: Roles.EDITOR,
-    pass: editorPass
-  },
-  [Roles.ADMIN]: {
-    role: Roles.ADMIN,
-    pass: adminPass
-  }
-}
 
 export const SHOW_ROLES: Record<Roles, string> = {
   [Roles.USER]: 'Usuario',
