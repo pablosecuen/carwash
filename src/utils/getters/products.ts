e0ae15214a6e859ef6e456a00f95ddc8cb0913f8
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-07 13:39:26 -0300

    feat: update getters and add product by id

--- src/utils/getters/products.ts
+++ src/utils/getters/products.ts
@@ -1,5 +1,12 @@
+import { type Product } from '@/db/entities/product'
 import { productRepository } from '@/db/repositories/product'
 
 export async function getAllProducts() {
-  return await productRepository.findAll()
+  const products = await productRepository.findAll()
+  return JSON.parse(JSON.stringify(products)) as Product[]
+}
+
+export async function getProductById(id: number) {
+  const product = await productRepository.findById(id)
+  return JSON.parse(JSON.stringify(product)) as Product
 }
