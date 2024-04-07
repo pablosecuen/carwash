26e61e0a6df83004aa5296b62807513bf34f8e5b
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-07 17:12:47 -0300

    feat: formulario de login con validacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { loginForm } from '@/actions/login-form'
 import { ToggleTheme } from '@/components/layout'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
-import { SHOW_ROLES } from '@/utils/constants'
+import { LoginForm } from '@/components/login/LoginForm'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-sm flex items-center justify-end'>
+      <div className='flex items-center justify-end sm:mx-auto sm:w-full sm:max-w-sm'>
         <ToggleTheme />
       </div>
       <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
@@ -30,33 +18,13 @@ export default function Home() {
         <h1 className='mt-4 text-center text-2xl font-bold leading-9 tracking-tight '>
           Bienvenido a Carwash
         </h1>
-        <p className='text-lg mt-2 text-center opacity-80'>
+        <p className='mt-2 text-center text-lg opacity-80'>
           Proveedor de servicios de lavado de coches.
         </p>
       </div>
 
       <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-sm'>
-        <form action={loginForm} className='space-y-2'>
-          <Select name='role'>
-            <SelectTrigger>
-              <SelectValue placeholder='Selecciona un rol' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                {Object.entries(SHOW_ROLES).map(([role, name]) => (
-                  <SelectItem key={role} value={role}>
-                    {name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <div className='flex w-full max-w-sm items-center space-x-2'>
-            <Input type='password' id='password' name='password' placeholder='Contraseña' />
-
-            <Button type='submit'>Ingresar</Button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </main>
   )
---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,103 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { loginForm } from '@/actions/login-form'
+import { SHOW_ROLES } from '@/utils/shared-constants'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form'
+import { toast } from '../ui/use-toast'
+
+const formSchema = z.object({
+  password: z.string().min(1, 'Este campo es requerido *'),
+  role: z.string().min(1, 'Este campo es requerido *')
+})
+
+export const LoginForm = () => {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: '',
+      role: ''
+    }
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const formData = new FormData()
+    formData.append('password', values.password)
+    formData.append('role', values.role)
+    const { ok, message } = await loginForm(formData)
+    if (!ok) {
+      toast({
+        variant: 'destructive',
+        title: message
+      })
+      return
+    }
+    router.push('/home')
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
+        <FormField
+          name='role'
+          control={form.control}
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder='Selecciona un rol' />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      {Object.entries(SHOW_ROLES).map(([role, name]) => (
+                        <SelectItem key={role} value={role}>
+                          {name}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                  <FormMessage className='text-sm' />
+                </Select>
+              </FormItem>
+            )
+          }}
+        />
+        <div className='flex w-full max-w-sm items-start space-x-2'>
+          <div className='flex-1'>
+            <FormField
+              name='password'
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder='Contraseña' />
+                    </FormControl>
+                    <FormMessage className='text-sm' />
+                  </FormItem>
+                )
+              }}
+            />
+          </div>
+
+          <Button type='submit'>Ingresar</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
