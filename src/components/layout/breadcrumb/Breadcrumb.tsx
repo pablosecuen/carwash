dd3ebfa194145f6ea1e240d6e604618d94e840df
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-04-08 16:12:43 -0300

    feat: traduccion de las rutas para los breadcrumbs

--- src/components/layout/breadcrumb/Breadcrumb.tsx
+++ src/components/layout/breadcrumb/Breadcrumb.tsx
@@ -13,16 +13,24 @@ interface Props {
   className?: string
 }
 
+const translationMap: { [key: string]: string } = {
+  products: 'Productos',
+  product: 'Producto',
+  service: 'Servicio',
+  'create-service': 'Crear Servicio'
+}
+
 export const Breadcrumbs = ({ className }: Props) => {
   const pathname = usePathname()
 
   const getBradcrumbArr = (pathname: string) => {
     const pathArr = pathname.split('/')
     const breadcrumb = pathArr.map((item, index) => {
+      const translatedName = translationMap[item] || item
       const path = pathArr.slice(0, index + 1).join('/')
       return {
         path,
-        name: item
+        name: translatedName
       }
     })
     return breadcrumb
