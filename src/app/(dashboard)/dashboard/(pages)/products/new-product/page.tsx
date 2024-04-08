895c7423dec93cd8e898204d9169e621c165637a
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-04-08 16:38:10 -0300

    feat: add new product page

---
+++ src/app/(dashboard)/dashboard/(pages)/products/new-product/page.tsx
@@ -0,0 +1,10 @@
+import { Title } from '@/components/layout'
+import { ContainerPage } from '@/components/layout/page/ContainerPage'
+
+export default function NewProductPage() {
+  return (
+    <ContainerPage>
+      <Title title='Nuevo producto' />
+    </ContainerPage>
+  )
+}
