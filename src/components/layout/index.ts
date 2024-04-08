1dfb541742ed07d129694f92a2289571227522a0
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-08 10:57:40 -0300

    fix: delete sidebaritem component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,5 @@
 export * from './header/Header'
 export * from './sidebar/Sidebar'
-export * from './sidebar/SidebarItem'
 export * from './sidebar/mobile-sidebar'
 export * from './theme/ToggleTheme'
 export * from './dashboard-nav'
