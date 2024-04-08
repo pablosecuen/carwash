6a89086a982837c4b89aa2a1d47ed18218f0a5eb
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-08 19:03:45 -0300

    feat: tabla de productos

--- src/app/(dashboard)/dashboard/(pages)/products/page.tsx
+++ src/app/(dashboard)/dashboard/(pages)/products/page.tsx
@@ -2,6 +2,10 @@ import { Title } from '@/components/layout'
 import { ContainerPage } from '@/components/layout/page/ContainerPage'
 import { getAllProducts } from '@/utils/getters/products'
 import { ProductsEmpty } from './ui/ProductsEmpty'
+import { ProductsTable } from './ui/ProductsTable'
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
 
 export default async function ProductsPage() {
   const products = await getAllProducts()
@@ -17,7 +21,17 @@ export default async function ProductsPage() {
   }
   return (
     <ContainerPage>
-      <Title title='Productos' />
+      <header className='flex items-center justify-between'>
+        <Title title='Productos' />
+        <Button size='sm' asChild>
+          <Link href={'/dashboard/products/new-product'} className='h-8 gap-1'>
+            <PlusCircle className='h-3.5 w-3.5' />
+            <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Crear producto</span>
+          </Link>
+        </Button>
+      </header>
+
+      <ProductsTable products={products} />
     </ContainerPage>
   )
 }
---
+++ src/app/(dashboard)/dashboard/(pages)/products/ui/ProductsTable.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image'
+import { MoreHorizontal } from 'lucide-react'
+
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+import { Product } from '@/db/entities/product'
+
+interface Props {
+  products: Product[]
+}
+
+export function ProductsTable({ products }: Props) {
+  return (
+    <Card>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead className='hidden md:table-cell'>Precio efectivo</TableHead>
+              <TableHead className='hidden md:table-cell'>Precio Tarjeta</TableHead>
+              <TableHead>
+                <span className='sr-only'>Acciones</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {products.map(({ name, cardPrice, cashPrice, id }) => (
+              <TableRow key={id}>
+                <TableCell className='font-medium'>{name}</TableCell>
+                <TableCell className='hidden md:table-cell'>{cashPrice}</TableCell>
+                <TableCell className='hidden md:table-cell'>{cardPrice}</TableCell>
+
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button aria-haspopup='true' size='icon' variant='ghost'>
+                        <MoreHorizontal className='h-4 w-4' />
+                        <span className='sr-only'>Toggle menu</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align='end'>
+                      <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                      <DropdownMenuItem>Editar</DropdownMenuItem>
+                      <DropdownMenuItem>Eliminar</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+      <CardFooter>
+        <div className='text-xs text-muted-foreground'>
+          Showing <strong>1-10</strong> of <strong>32</strong> products
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
