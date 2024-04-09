2773ff3e2f4410695b92424d84a9c1379d676be9
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-09 12:01:37 -0300

    feat: add currency format para los precios

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const currencyFormat = (value: number) => {
+  // Utilizamos Intl.NumberFormat con el código de idioma y la configuración de moneda de Argentina
+  const formatter = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  })
+
+  // Aplicamos el formato al valor numérico y lo retornamos
+  return formatter.format(value)
 }
