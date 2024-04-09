ce7fdda8b198ac2101831b8d727ff28634c031df
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-09 17:00:52 -0300

    feat: add getter all customer

--- src/utils/getters/customer.ts
+++ src/utils/getters/customer.ts
@@ -1,5 +1,9 @@
 import { customerRepository } from '@/db/repositories/customer'
 
+export async function getAllCustomers() {
+  return await customerRepository.findAll()
+}
+
 // TODO: hanlder error properly
 export async function getCustomerByName(name?: string) {
   return name != null ? await customerRepository.findByName(name) : []
