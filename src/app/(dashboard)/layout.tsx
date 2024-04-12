8696b26a7331630201ddf9b495ae0e910ab87ee9
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-11 21:17:52 -0300

    feat: add validation user role

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,12 @@
 import { Header } from '@/components/layout/header/Header'
 import { Sidebar } from '@/components/layout/sidebar/Sidebar'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const role = cookies().get('role')
+  if (role == null) redirect('/')
+
   return (
     <>
       <Header />
