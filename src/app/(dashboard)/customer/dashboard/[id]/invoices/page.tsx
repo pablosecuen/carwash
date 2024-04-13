0c54ede4630e156a0f12fd8941cb69b7bacc0824
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-13 14:25:28 -0300

    feat: styles invoices table

--- src/app/(dashboard)/customer/dashboard/[id]/invoices/page.tsx
+++ src/app/(dashboard)/customer/dashboard/[id]/invoices/page.tsx
@@ -1,6 +1,15 @@
 import { dateFormater } from '@/utils/formatters'
 import { getInvoicesByCustomerId } from '@/utils/getters/invoices'
 import { RangeDate } from './ui/range-date'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 
 interface Props {
   params: {
@@ -20,28 +29,32 @@ export default async function Page({ params, searchParams }: Props) {
     to: to != null ? new Date(to) : undefined
   })
   return (
-    <section>
-      <RangeDate />
-      <table>
-        <thead>
-          <tr>
-            <th>Fecha</th>
-            <th>Estado</th>
-            <th>Total</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {invoices.map((invoice) => (
-            <tr key={invoice.id}>
-              <td>{dateFormater(new Date(invoice.createAt))}</td>
-              <td>{invoice.status}</td>
-              <td>{invoice.total}</td>
-              <td> - </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
+    <Card className='fade-in'>
+      <CardHeader>
+        <RangeDate />
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Fecha</TableHead>
+              <TableHead>Estado</TableHead>
+              <TableHead>Total</TableHead>
+              <TableHead>Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.id}>
+                <TableCell>{dateFormater(new Date(invoice.createAt))}</TableCell>
+                <TableCell>{invoice.status}</TableCell>
+                <TableCell>{invoice.total}</TableCell>
+                <TableCell> - </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   )
 }
