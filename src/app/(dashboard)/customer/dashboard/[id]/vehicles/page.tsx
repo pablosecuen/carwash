83652071c2f7aa0b5eaa1048b8580536d7137768
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-13 13:44:08 -0300

    feat: add styles customer vehicles table

--- src/app/(dashboard)/customer/dashboard/[id]/vehicles/page.tsx
+++ src/app/(dashboard)/customer/dashboard/[id]/vehicles/page.tsx
@@ -1,32 +1,49 @@
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 import { getByCustomerId } from '@/utils/getters/vehicles'
+import { ExternalLink } from 'lucide-react'
 import Link from 'next/link'
 
 export default async function Page({ params }: { params: { id: string } }) {
   const vehicles = await getByCustomerId(Number(params.id))
   return (
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>Marca</th>
-            <th>Modelo</th>
-            <th>Patente</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {vehicles.map((vehicle) => (
-            <tr key={vehicle.id}>
-              <td>{vehicle.brand}</td>
-              <td>{vehicle.model}</td>
-              <td>{vehicle.patent}</td>
-              <td className='mx-2 flex gap-4'>
-                <Link href={`/customer/dashboard/${vehicle.id}/vehicles/tickets`}>tickets</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
+    <Card className='fade-in'>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Marca</TableHead>
+              <TableHead>Modelo</TableHead>
+              <TableHead>Patente</TableHead>
+              <TableHead>Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {vehicles.map((vehicle) => (
+              <TableRow key={vehicle.id}>
+                <TableCell>{vehicle.brand}</TableCell>
+                <TableCell>{vehicle.model}</TableCell>
+                <TableCell>{vehicle.patent}</TableCell>
+                <TableCell className='mx-2 flex gap-4'>
+                  <Link
+                    href={`/customer/dashboard/${vehicle.id}/vehicles/tickets`}
+                    className='flex items-center gap-2'
+                  >
+                    Tickets <ExternalLink size={18} />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   )
 }
