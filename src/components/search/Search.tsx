f91476bebc63da3bca0b9646a97cfc5b1a357056
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-15 14:34:48 -0300

    fix: input search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -3,6 +3,7 @@
 import { SearchCheck } from 'lucide-react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
+import { Input } from '../ui/input'
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
@@ -23,15 +24,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <label htmlFor='search' className='sr-only'>
         Search
       </label>
-      <input
-        className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+      <Input
+        className='peer block w-full  py-[9px] pl-10 text-sm outline-2 '
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value)
         }}
         defaultValue={searchParams.get('query')?.toString()}
       />
-      <SearchCheck className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+      <SearchCheck className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2  text-muted-foreground' />
     </div>
   )
 }
