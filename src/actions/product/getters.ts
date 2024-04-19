221976f6752aa736648518c13568eb7b9ce59cfa
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-19 18:11:19 -0300

    feat: pedido de productos con formateo para retornar datos necesarios para el front

--- src/actions/product/getters.ts
+++ src/actions/product/getters.ts
@@ -1,9 +1,25 @@
+'use server'
 import { type Product } from '@/db/entities/product'
 import { productRepository } from '@/db/repositories/product'
 
 export async function getAllProducts() {
-  const products = await productRepository.findAll()
-  return JSON.parse(JSON.stringify(products)) as Product[]
+  try {
+    const products = await productRepository.findAll()
+    return JSON.parse(
+      JSON.stringify(
+        products.map((product) => {
+          return {
+            name: product.name,
+            cashPrice: product.cashPrice,
+            cardPrice: product.cardPrice
+          }
+        })
+      )
+    ) as Product[]
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
 
 export async function getProductById(id: number) {
