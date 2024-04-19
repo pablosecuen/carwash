8b51c13b19270d261c7514303ccc221410f77d97
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-19 20:37:42 -0300

    feat: new ticket page

--- src/app/(dashboard)/service/(pages)/new-ticket/page.tsx
+++ src/app/(dashboard)/service/(pages)/new-ticket/page.tsx
@@ -1,20 +1,15 @@
 import { Title } from '@/components/layout'
 import { ContainerPage } from '@/components/layout/page/ContainerPage'
+import { TicketForm } from './ui/TicketForm'
 
 export default function NamePage() {
   return (
     <ContainerPage>
       <header className='flex items-center justify-between fade-in'>
         <Title title='Nuevo ticket' />
-        {/* <Button size='sm' asChild>
-          <Link href={'/dashboard/products/new-product'} className='h-8 gap-1'>
-            <PlusCircle className='h-3.5 w-3.5' />
-            <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Crear producto</span>
-          </Link>
-        </Button> */}
       </header>
 
-      {/* <AddCustomer /> */}
+      <TicketForm />
     </ContainerPage>
   )
 }
---
+++ src/app/(dashboard)/service/(pages)/new-ticket/ui/TicketForm.tsx
@@ -0,0 +1,9 @@
+import { Card, CardContent } from '@/components/ui/card'
+
+export const TicketForm = () => {
+  return (
+    <Card className='mt-5 flex w-full flex-col gap-4 rounded-lg bg-muted/50 p-5'>
+      <CardContent></CardContent>
+    </Card>
+  )
+}
