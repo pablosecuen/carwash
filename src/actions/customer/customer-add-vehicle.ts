de08d4e9332ad6ef2beca3b890543564511de820
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-19 20:37:23 -0300

    fix: accion para crear vehiculo a un customer modificada para recibir dos parametros

--- src/actions/customer/customer-add-vehicle.ts
+++ src/actions/customer/customer-add-vehicle.ts
@@ -1,29 +1,27 @@
+'use server'
 import { customerRepository } from '@/db/repositories/customer'
 import { vehicleRepository } from '@/db/repositories/vehicle'
 import { type VehicleType } from '@/utils/types'
 
-export async function customerAddVehicle(customerId: number) {
-  return async function (formData: FormData) {
-    'use server'
-    // TODO: validate formData
-    const data = {
-      type: formData.get('type') as VehicleType,
-      brand: formData.get('brand') as string,
-      model: formData.get('model') as string,
-      year: Number(formData.get('year')),
-      patent: formData.get('patent') as string,
-      customerId
+export async function customerAddVehicle(customerId: number, formData: FormData) {
+  // TODO: validate formData
+  const data = {
+    type: formData.get('type') as VehicleType,
+    brand: formData.get('brand') as string,
+    model: formData.get('model') as string,
+    year: Number(formData.get('year')),
+    patent: formData.get('patent') as string,
+    customerId
+  }
+  try {
+    // TODO: handler possible errors
+    const vehicle = await vehicleRepository.create(data)
+    if (vehicle == null) {
+      throw new Error('Error creating vehicle')
     }
-    try {
-      // TODO: handler possible errors
-      const vehicle = await vehicleRepository.create(data)
-      if (vehicle == null) {
-        throw new Error('Error creating vehicle')
-      }
-      const customer = await customerRepository.addVehicle(customerId, vehicle)
-      console.log({ customer, vehicle })
-    } catch (error) {
-      console.error('Error adding vehicle to customer:', error)
-    }
+    const customer = await customerRepository.addVehicle(customerId, vehicle)
+    console.log({ customer, vehicle })
+  } catch (error) {
+    console.error('Error adding vehicle to customer:', error)
   }
 }
