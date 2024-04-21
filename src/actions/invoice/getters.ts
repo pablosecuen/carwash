4cc4c67ede921eeeed275cdfbc9bae878e0fce52
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-21 18:04:47 -0300

    feat: add getter daily invoices

--- src/actions/invoice/getters.ts
+++ src/actions/invoice/getters.ts
@@ -1,8 +1,24 @@
 import { invoiceRepository } from '@/db/repositories/invoice'
+import { type Branch } from '@/utils/types'
+import { cookies } from 'next/headers'
 
 export async function getInvoicesByCustomerId(
   customerId: string,
   options: { from?: Date; to?: Date } = {}
 ) {
   return await invoiceRepository.findByCustomerId(customerId, options)
+}
+
+export const getDailyInvoices = async () => {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  const tomorrow = new Date(today)
+  tomorrow.setDate(tomorrow.getDate() + 1)
+  const branch = cookies().get('branch')?.value as Branch
+  const invoices = await invoiceRepository.findAll({
+    branch,
+    from: today,
+    to: tomorrow
+  })
+  return JSON.parse(JSON.stringify(invoices)) as typeof invoices
 }
