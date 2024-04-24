c1d496f37df6c4561f463ceb0d813bba375b3e57
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-04-24 18:59:23 -0300

    Tipado de producto

--- src/actions/product/getters.ts
+++ src/actions/product/getters.ts
@@ -2,16 +2,19 @@
 import { type Product } from '@/db/entities/product'
 import { productRepository } from '@/db/repositories/product'
 
+
+
 export async function getAllProducts() {
   try {
     const products = await productRepository.findAll()
     return JSON.parse(
       JSON.stringify(
-        products.map((product) => {
+        products.map((product: Product) => {
           return {
             name: product.name,
             cashPrice: product.cashPrice,
-            cardPrice: product.cardPrice
+            cardPrice: product.cardPrice,
+            updatedAt: product.updatedAt
           }
         })
       )
