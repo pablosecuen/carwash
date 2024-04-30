0105b46032df553404b836b8cdcbd42ec872eec9
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-29 21:47:49 -0300

    feat: add branch to create customer action

--- src/actions/customer/create-customer.ts
+++ src/actions/customer/create-customer.ts
@@ -1,19 +1,21 @@
 'use server'
 import { customerRepository } from '@/db/repositories/customer'
+import { getBranch } from '@/utils/user-validate'
 import { revalidatePath } from 'next/cache'
 
 export async function createCustomerAction(formData: FormData) {
   // TODO: Validate form data
   const name = formData.get('name') as string
-  const data = {
-    name: formData.get('name') as string,
-    email: formData.get('email') as string,
-    phone: formData.get('phone') as string,
-    address: formData.get('address') as string,
-    currentAccount: false,
-    slug: name.replace(/ /g, '_').toLocaleLowerCase()
-  }
   try {
+    const data = {
+      name,
+      email: formData.get('email') as string,
+      phone: formData.get('phone') as string,
+      address: formData.get('address') as string,
+      currentAccount: false,
+      slug: name.replace(/ /g, '_').toLocaleLowerCase(),
+      branch: getBranch()
+    }
     const customer = await customerRepository.create(data)
     console.log('Customer created:', customer)
 
