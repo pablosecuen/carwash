2b46841a891efad9c93af7e3288e574444341daf
Franco de Desarrollo <francoegi01@gmail.com>
2024-04-30 22:39:42 -0300

    feat: filter customer by branch no admin users and handle error

--- src/actions/customer/getters.ts
+++ src/actions/customer/getters.ts
@@ -1,8 +1,22 @@
 import { type Customer } from '@/db/entities'
 import { customerRepository } from '@/db/repositories/customer'
+import { getBranch, hasPermission } from '@/utils/user-validate'
 
 export async function getAllCustomers() {
-  return JSON.parse(JSON.stringify(await customerRepository.findAll())) as Customer[]
+  try {
+    const isAdmin = await hasPermission('ADMIN')
+    return JSON.parse(
+      JSON.stringify(
+        await customerRepository.findAll({
+          // filter by branch if user is not admin
+          branch: isAdmin ? undefined : getBranch()
+        })
+      )
+    ) as Customer[]
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
 
 // TODO: hanlder error properly
