d9864bd48fd208b0480002c9077e9eebfffbb4e3
Franco de Desarrollo <francoegi01@gmail.com>
2024-05-01 11:45:42 -0300

    feat: add class clousures find all

--- src/db/repositories/cash-clousures.ts
+++ src/db/repositories/cash-clousures.ts
@@ -1,5 +1,12 @@
+import { type Branch } from '@/utils/types'
 import { CashClosures } from './../entities/cash-closures'
 import { BaseRepository } from './base-repository'
+
+interface FilterOpts {
+  branch?: Branch
+  limit?: number
+  offset?: number
+}
 
 export class CashClosuresRepository extends BaseRepository<CashClosures> {
   protected entity = CashClosures
@@ -11,6 +18,21 @@ export class CashClosuresRepository extends BaseRepository<CashClosures> {
     })
     await this.repository.save(cashClosure)
     return cashClosure
+  }
+
+  async findAll(opts: FilterOpts = {}) {
+    const { branch, limit = 20, offset = 0 } = opts
+    await this.init()
+    return await this.repository.find({
+      where: {
+        branch
+      },
+      order: {
+        createdAt: 'DESC'
+      },
+      take: limit,
+      skip: offset
+    })
   }
 }
 
