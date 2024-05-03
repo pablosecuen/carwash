cab9f53cd25020443e49afa28c2ff11d9ec628a3
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-03 15:19:37 -0300

    feat: info de user en header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,8 +2,12 @@ import { cn } from '@/lib/utils'
 import { ToggleTheme } from '../theme/ToggleTheme'
 import { LogOut } from './logout'
 import { MobileSidebarServer } from '../sidebar/mobile-sidebar-server'
+import { getUserRole } from '@/utils/user-validate'
+import { cookies } from 'next/headers'
 
-export const Header = () => {
+export const Header = async () => {
+  const role = await getUserRole()
+  const branch = cookies().get('branch')?.value
   return (
     <div className='supports-backdrop-blur:bg-background/60  fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur'>
       <nav className='flex h-14 items-center justify-between px-4'>
@@ -12,9 +16,15 @@ export const Header = () => {
           <img className=' h-10 w-auto' src='/carwash-logo.webp' alt='Your Company' />
 
           <h1 className='text-xl font-semibold tracking-wide'>Carwash</h1>
+          <small>
+            Ingreso como {role}, en sucursal {branch}
+          </small>
         </div>
-        <div className={cn('block lg:!hidden')}>
+        <div className={cn('flex flex-1 items-center gap-5 lg:!hidden')}>
           <MobileSidebarServer />
+          <small>
+            Ingreso como {role}, en sucursal {branch}
+          </small>
         </div>
 
         <div className='flex items-center gap-2'>
