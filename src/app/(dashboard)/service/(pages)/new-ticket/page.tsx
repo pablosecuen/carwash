97128ddb4556269948d24616ef10ccc0c8f84927
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-04 14:56:42 -0300

    fix: reestructuracion de componente

--- src/app/(dashboard)/service/(pages)/new-ticket/page.tsx
+++ src/app/(dashboard)/service/(pages)/new-ticket/page.tsx
@@ -1,28 +1,33 @@
 import { Title } from '@/components/layout'
 import { ContainerPage } from '@/components/layout/page/ContainerPage'
-import { getCustomersByName } from '@/actions/customer/getters'
+
 import { getAllProducts } from '@/actions/product/getters'
 import { getAllServices } from '@/actions/service/getters'
 import { getByCustomerId } from '@/actions/vehicle/getters'
-import { ClientField } from './ui/client-field'
+
 import { InvoiceForm } from './ui/invoice-form'
+import { CustomerFieldServer } from './ui/customer-field.server'
+import { Suspense } from 'react'
 
 interface SearchParams {
   customerName?: string
   customerId?: string
 }
 export default async function NamePage({ searchParams }: { searchParams: SearchParams }) {
-  const customers = await getCustomersByName(searchParams.customerName)
   const services = await getAllServices()
   const vehicles = await getByCustomerId(Number(searchParams.customerId))
   const products = await getAllProducts()
+
+  const { customerName } = searchParams
   return (
     <ContainerPage>
       <header className='flex items-center justify-between fade-in'>
         <Title title='Nuevo ticket' />
       </header>
       <div className='grid w-max grid-cols-3 place-content-center items-start gap-5'>
-        <ClientField customers={customers} />
+        <Suspense fallback={<></>}>
+          <CustomerFieldServer customerName={customerName ?? ''} />
+        </Suspense>
       </div>
 
       <InvoiceForm {...{ services, vehicles, products, customerId: searchParams.customerId }} />
