00602f7cd4fd91e0bdfaec92511e6258336b1ec7
Franco de Desarrollo <francoegi01@gmail.com>
2024-05-05 21:22:02 -0300

    fix: add error handling to product seeds route

--- src/app/api/seeds/product/route.ts
+++ src/app/api/seeds/product/route.ts
@@ -4,11 +4,18 @@ import { NextResponse } from 'next/server'
 let isCreated = false
 
 export async function GET() {
-  if (!isCreated && process.env.NODE_ENV === 'development') {
-    await createProducts()
-    isCreated = true
+  try {
+    if (!isCreated && process.env.NODE_ENV === 'development') {
+      await createProducts()
+      isCreated = true
+    }
+    return NextResponse.json({
+      message: 'Products created successfully!'
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({
+      message: 'Error creating products!'
+    })
   }
-  return NextResponse.json({
-    message: 'Products created successfully!'
-  })
 }
