36960a27269455aa7d87a1624e0493b2e0f3cf16
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-07 16:37:54 -0300

    fix: fix

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,39 +1,36 @@
-import { ContainerPage } from '@/components/layout/page/ContainerPage'
-
-import { DashboardCard } from '@/components/DashboardCard'
-
 import { redirect } from 'next/navigation'
+import { ContainerPage } from '@/components/layout/page/ContainerPage'
+import { DashboardCard } from '@/components/DashboardCard'
 import { hasPermission } from '@/utils/user-validate'
 
 export default async function DashboardPage() {
   const isAdmin = await hasPermission()
+
   if (!isAdmin) {
     redirect('/')
   }
-  // const products = await getAllProducts()
+
   return (
     <ContainerPage>
       <div className='grid flex-1 items-start gap-4  md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
-        {/* left */}
         <div className='grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3'>
-          {/* Cards */}
-          <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6'>
+          <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6'>
             <DashboardCard
-              className='sm:col-span-2 '
+              className='sm:col-span-2'
               title='Facturas'
               description='Esta sección te permite administrar las facturas que se ofrecen en el sistema.'
               href='/dashboard/invoices'
               linkLabel='Ir a facturas'
             />
             <DashboardCard
-              className='sm:col-span-2 '
+              className='sm:col-span-2'
               title='Servicios'
               description='Esta sección te permite administrar los servicios que se ofrecen en el sistema.'
               href='/dashboard/services'
               linkLabel='Ir a servicios'
             />
             <DashboardCard
-              className='sm:col-span-2 md:col-span-full lg:col-span-2 '
+              className='sm:col-span-2 md:col-span-full lg:col-span-2'
               title='Productos'
               description='Esta sección te permite administrar los productos que se ofrecen en el sistema.'
               href='/dashboard/products'
--- src/app/(dashboard)/service/page.tsx
+++ src/app/(dashboard)/service/page.tsx
@@ -6,20 +6,20 @@ import { TableDailyInvoices } from './ui/TableDailyInvoices'
 export default async function ServicePage() {
   return (
     <ContainerPage>
-      <div className='grid flex-1 items-start gap-4  md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
+      <div className='grid flex-1 items-start gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3'>
         {/* left */}
         <div className='grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-3'>
           {/* Cards */}
           <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6'>
             <DashboardCard
-              className=' sm:col-span-2 '
+              className=' sm:col-span-2'
               title='Tickets'
               description='Esta sección te permite crear tickets para registrar en el sistema.'
               href='/service/new-ticket'
               linkLabel='Crear ticket'
             />
             <DashboardCard
-              className=' sm:col-span-2 '
+              className=' sm:col-span-2'
               title='Clientes'
               description='Esta sección te permite administrar los clientes del sistema.'
               href='/service/customers'
@@ -27,7 +27,7 @@ export default async function ServicePage() {
             />
 
             <DashboardCardProducts
-              className=' sm:col-span-2 md:col-span-full lg:col-span-2'
+              className='sm:col-span-2 md:col-span-full lg:col-span-2'
               title='Productos'
               description='Aqui podras ver todos los productos que hay en el sistema.'
             />
