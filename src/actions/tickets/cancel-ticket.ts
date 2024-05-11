d7c90c171124c64c8ea28f11eddf95e93f552ac9
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-11 20:02:56 -0300

    feat: add revalidation after ticket status update

--- src/actions/tickets/cancel-ticket.ts
+++ src/actions/tickets/cancel-ticket.ts
@@ -2,10 +2,12 @@
 
 import { ticketRepository } from '@/db/repositories/ticket'
 import { TicketStatus } from '@/utils/types'
+import { revalidatePath } from 'next/cache'
 
 export async function cancelTicket({ id }: { id: number | string }) {
   try {
     await ticketRepository.updateStatus({ id: Number(id), status: TicketStatus.CANCELLED })
+    revalidatePath('/service')
     return {
       ok: true,
       message: 'Ticket cancelled successfully'
