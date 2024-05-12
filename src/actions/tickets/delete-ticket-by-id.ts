e5ee2a21c5451d99adf411fabc782acfcfc6a67a
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-05-12 03:20:34 -0300

    feat: implement deleteTicketById function

---
+++ src/actions/tickets/delete-ticket-by-id.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { ticketRepository } from '@/db/repositories/ticket'
+
+export const deleteTicketById = async (id: string) => {
+  try {
+    await ticketRepository.deleteById(Number(id))
+    return { ok: true, message: 'Ticket deleted' }
+  } catch (error) {
+    return { ok: false, message: 'Ticket no eliminado' }
+  }
+}
