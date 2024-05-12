b21faac50ec95cf180e8b07f91eef07fe3a447a8
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-05-12 18:15:45 -0300

    Update deleteTicketById function to return a localized message

--- src/actions/tickets/delete-ticket-by-id.ts
+++ src/actions/tickets/delete-ticket-by-id.ts
@@ -5,7 +5,7 @@ import { ticketRepository } from '@/db/repositories/ticket'
 export const deleteTicketById = async (id: string) => {
   try {
     await ticketRepository.deleteById(Number(id))
-    return { ok: true, message: 'Ticket deleted' }
+    return { ok: true, message: 'Ticket eliminado' }
   } catch (error) {
     return { ok: false, message: 'Ticket no eliminado' }
   }
