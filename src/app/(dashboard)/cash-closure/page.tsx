e92204bf9583e473796e091532737aa8623bb8a5
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-05-12 18:15:52 -0300

    Refactor cash closure page layout and components

--- src/app/(dashboard)/cash-closure/page.tsx
+++ src/app/(dashboard)/cash-closure/page.tsx
@@ -1,46 +1,93 @@
 import { createCashClosureAction } from '@/actions/cash-closures/create'
 import { getDailyInvoices } from '@/actions/invoice/getters'
+import { ContainerPage } from '@/components/layout/page/ContainerPage'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+import { currencyFormat, dateFormat, variantBadge } from '@/lib/utils'
+import { translateStatus } from '@/utils/formatters'
 
 export default async function Page() {
   const invoices = await getDailyInvoices()
   const total = invoices.reduce((acc, invoice) => acc + invoice.total, 0)
   const paymentToEmployees = Math.round(total * 0.3)
   return (
-    <div>
-      <h1>Cierre de caja</h1>
-      <p>Contenido de la página de cierre de caja</p>
-      <ul>
-        {invoices.map((invoice) => (
-          <li key={invoice.id}>
-            <p>{new Date(invoice.createAt).toISOString()}</p>
-            <p>{invoice.total}</p>
-          </li>
-        ))}
-      </ul>
+    <ContainerPage>
+      <Card className='border-none'>
+        <CardHeader>
+          <CardTitle>Cierre de caja</CardTitle>
+          <CardDescription>Contenido de la página de cierre de caja</CardDescription>
+        </CardHeader>
+        <CardContent className='grid gap-16 space-y-16 lg:grid-cols-2'>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>ID</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead>Fecha</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Total</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {invoices.map(({ id, customer, total, createAt, status }) => (
+                <TableRow key={id}>
+                  <TableCell>{id}</TableCell>
+                  <TableCell>{customer.name}</TableCell>
+                  <TableCell>{dateFormat(new Date(createAt))}</TableCell>
+                  <TableCell>
+                    <Badge variant={variantBadge(status)}>{translateStatus(status)}</Badge>
+                  </TableCell>
+                  <TableCell>{currencyFormat(total)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={4}>Total</TableCell>
+                <TableCell>{currencyFormat(total)}</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
 
-      <form action={createCashClosureAction} className='grid '>
-        <label htmlFor='totalDaily'>
-          Monto en total caja
-          <input name='totalDaily' type='number' value={total} />
-        </label>
-        <label htmlFor='dailyPercentage'>
-          Porcentaje del día
-          <input name='dailyPercentage' type='number' defaultValue={30} />
-        </label>
-        <label htmlFor='managerBonus'>
-          Bonus al gerente
-          <input name='managerBonus' type='number' />
-        </label>
-        <label htmlFor='employeeBonus'>
-          Bonus a los empleados
-          <input name='employeeBonus' type='number' defaultValue={0} />
-        </label>
-        <label htmlFor='employeePayment'>
-          Pago a empleados
-          <input name='employeePayment' type='number' defaultValue={paymentToEmployees} />
-        </label>
-        <button type='submit'>Cerrar caja</button>
-      </form>
-    </div>
+          <form action={createCashClosureAction} className='grid space-y-3 '>
+            <Label htmlFor='totalDaily'>
+              Monto en total caja
+              <Input name='totalDaily' type='number' value={total} />
+            </Label>
+            <Label htmlFor='dailyPercentage'>
+              Porcentaje del día
+              <Input name='dailyPercentage' type='number' defaultValue={30} />
+            </Label>
+            <Label htmlFor='managerBonus'>
+              Bonus al gerente
+              <Input name='managerBonus' type='number' />
+            </Label>
+            <Label htmlFor='employeeBonus'>
+              Bonus a los empleados
+              <Input name='employeeBonus' type='number' defaultValue={0} />
+            </Label>
+            <Label htmlFor='employeePayment'>
+              Pago a empleados
+              <Input name='employeePayment' type='number' defaultValue={paymentToEmployees} />
+            </Label>
+            <Button variant={'secondary'} type='submit'>
+              Cerrar caja
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </ContainerPage>
   )
 }
