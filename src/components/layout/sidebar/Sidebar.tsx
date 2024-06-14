066bb10e19ab3fa7e497e6049d6e51a6a0225533
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-06-14 20:04:41 -0300

    fix: Add "Ayuda" link to sidebar in mobile view

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -2,17 +2,34 @@ import { DashboardNav } from '@/components/layout'
 import { navItems } from '@/lib/data'
 import { cn } from '@/lib/utils'
 import { getUserRole } from '@/utils/user-validate'
+import { HelpCircleIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export async function Sidebar() {
   const role = await getUserRole()
   return (
     <nav className={cn(`relative hidden min-h-screen w-72  border-r lg:block`)}>
-      <div className='space-y-4 py-4'>
-        <div className='px-3 py-2'>
+      <div className='flex h-full flex-col items-start justify-between space-y-4 py-4'>
+        <div className='w-full px-3 py-2'>
           <div className='space-y-1'>
             <h2 className='mb-2 px-4 text-xl font-semibold tracking-tight'>Carwash</h2>
             <DashboardNav items={navItems} role={role} />
           </div>
+        </div>
+        <div className='w-full space-y-4 px-3'>
+          <Link href={'/'} className={''}>
+            <span
+              className={
+                'group flex flex-col items-start rounded-md px-3 py-2 text-sm font-medium transition-all duration-75 hover:bg-accent hover:text-accent-foreground'
+              }
+            >
+              <div className='flex items-center'>
+                <HelpCircleIcon className='mr-2 h-4 w-4' />
+                <span>Ayuda</span>
+              </div>
+              <small>Blog sobre el sistema.</small>
+            </span>
+          </Link>
         </div>
       </div>
     </nav>
--- src/components/layout/sidebar/mobile-sidebar.tsx
+++ src/components/layout/sidebar/mobile-sidebar.tsx
@@ -2,11 +2,12 @@
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 
-import { MenuIcon } from 'lucide-react'
+import { HelpCircleIcon, MenuIcon } from 'lucide-react'
 import { useState } from 'react'
 import { DashboardNav } from '../dashboard-nav'
 import { navItems } from '@/lib/data'
 import { type Roles } from '@/utils/types'
+import Link from 'next/link'
 
 // import { Playlist } from "../data/playlists";
 
@@ -22,14 +23,35 @@ export function MobileSidebar({ role }: SidebarProps) {
         <SheetTrigger asChild>
           <MenuIcon />
         </SheetTrigger>
-        <SheetContent side='left' className='!px-0'>
-          <div className='space-y-4 py-4'>
+        <SheetContent side='left' className='flex flex-col items-start justify-between !px-0'>
+          <div className='w-full space-y-4 py-4'>
             <div className='px-3 py-2'>
-              <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>Overview</h2>
+              <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>Carwash</h2>
               <div className='space-y-1'>
                 <DashboardNav items={navItems} setOpen={setOpen} role={role} />
               </div>
             </div>
+          </div>
+          <div className='w-full space-y-4 px-3'>
+            <Link
+              href={'/'}
+              onClick={() => {
+                if (setOpen != null) setOpen(false)
+              }}
+              className={''}
+            >
+              <span
+                className={
+                  'group flex flex-col items-start rounded-md px-3 py-2 text-sm font-medium transition-all duration-75 hover:bg-accent hover:text-accent-foreground'
+                }
+              >
+                <div className='flex items-center'>
+                  <HelpCircleIcon className='mr-2 h-4 w-4' />
+                  <span>Ayuda</span>
+                </div>
+                <small>Blog sobre el sistema.</small>
+              </span>
+            </Link>
           </div>
         </SheetContent>
       </Sheet>
