e77a8007f8ddd5704bc2bb9e8761aa7282d076dd
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-06-14 21:01:04 -0300

    feat: Add Article component for blog page

---
+++ src/app/(dashboard)/blog/component/Article.tsx
@@ -0,0 +1,23 @@
+interface ArticleProps {
+  title: string
+  description: string
+  urlVideo: string
+  id: string
+}
+
+export const Article = ({ title, urlVideo, description, id }: ArticleProps) => {
+  return (
+    <article id={id} className='mx-auto flex  w-full flex-col gap-5 py-20'>
+      <h2 className='text-xl font-semibold'>{title}</h2>
+      <p>{description}</p>
+      <iframe
+        width='100%'
+        height='100%'
+        src={urlVideo}
+        title='Aprendamos 2024 #3 - ELO 1000 - Apertura de arqueros'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        className='h-[600px] w-full rounded-lg'
+      ></iframe>
+    </article>
+  )
+}
