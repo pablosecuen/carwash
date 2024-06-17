255de98149ba55a612618bbef127b89772f609a5
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-06-17 16:28:20 -0300

    refactor: Update Article component to use Card component for styling and layout

--- src/app/(dashboard)/blog/component/Article.tsx
+++ src/app/(dashboard)/blog/component/Article.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
 interface ArticleProps {
   title: string
   description: string
@@ -7,17 +9,20 @@ interface ArticleProps {
 
 export const Article = ({ title, urlVideo, description, id }: ArticleProps) => {
   return (
-    <article id={id} className='mx-auto flex  w-full flex-col gap-5 pt-20'>
-      <h2 className='text-xl font-semibold'>{title}</h2>
-      <p className='opacity-80'>{description}</p>
-      <iframe
-        width='100%'
-        height='100%'
-        src={urlVideo}
-        title='Aprendamos 2024 #3 - ELO 1000 - Apertura de arqueros'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        className='h-[600px] w-full rounded-lg'
-      ></iframe>
-    </article>
+    <Card id={id} className='mx-auto mt-20  flex w-full flex-col gap-5 bg-muted shadow-xl'>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <iframe
+          width='100%'
+          height='100%'
+          src={urlVideo}
+          title='Aprendamos 2024 #3 - ELO 1000 - Apertura de arqueros'
+          className='h-[600px] w-full rounded-lg'
+        ></iframe>
+      </CardContent>
+    </Card>
   )
 }
