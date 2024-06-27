093ac3524013023cbf0d21a0103ca7577363c615
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-06-27 04:14:41 -0300

    refactor: Update video URLs in blogArticles

--- src/app/(dashboard)/blog/lib/data.blog.ts
+++ src/app/(dashboard)/blog/lib/data.blog.ts
@@ -3,30 +3,30 @@ export const blogArticles = [
     id: 'creacion-de-servicios',
     title: '1. Creación de servicios',
     description: 'Aprende cómo crear y gestionar servicios en nuestra plataforma.',
-    urlVideo: 'https://www.youtube.com/embed/9xa0F73zKB4?list=PLB1AJxrCNNIMcbl44TKx80I03jaNlQIE-'
+    urlVideo: '/videos/crear-servicios.mp4'
   },
   {
     id: 'creacion-de-productos',
     title: '2. Creación de productos',
     description: 'Guía paso a paso para la creación de nuevos productos.',
-    urlVideo: 'https://www.youtube.com/embed/9xa0F73zKB4?list=PLB1AJxrCNNIMcbl44TKx80I03jaNlQIE-'
+    urlVideo: '/videos/crear-productos.mp4'
   },
   {
     id: 'creacion-de-clientes-y-sus-autos',
     title: '3. Creación de clientes y sus autos',
     description: 'Tutorial sobre cómo registrar clientes y sus vehículos en el sistema.',
-    urlVideo: 'https://www.youtube.com/embed/9xa0F73zKB4?list=PLB1AJxrCNNIMcbl44TKx80I03jaNlQIE-'
+    urlVideo: '/videos/crear-clientes-autos.mp4'
   },
   {
     id: 'creacion-de-tickets',
     title: '4. Creación de tickets',
     description: 'Proceso para la creación y gestión de tickets.',
-    urlVideo: 'https://www.youtube.com/embed/9xa0F73zKB4?list=PLB1AJxrCNNIMcbl44TKx80I03jaNlQIE-'
+    urlVideo: '/videos/crear-tickets.mp4'
   },
   {
     id: 'cierre-de-caja',
     title: '5. Cierre de caja',
     description: 'Instrucciones para realizar el cierre de caja correctamente.',
-    urlVideo: 'https://www.youtube.com/embed/9xa0F73zKB4?list=PLB1AJxrCNNIMcbl44TKx80I03jaNlQIE-'
+    urlVideo: '/videos/cierre-caja.mp4'
   }
 ]
