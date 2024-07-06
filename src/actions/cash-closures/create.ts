9ed3bc76a272ea3b7e2f097a921fb5b78b0f1dc6
Franco de Desarrollo <francoegi01@gmail.com>
2024-07-06 16:59:31 -0300

    feat: Update createCashClosureAction to include additional form data

--- src/actions/cash-closures/create.ts
+++ src/actions/cash-closures/create.ts
@@ -7,15 +7,20 @@ import { getBranch } from '@/utils/user-validate'
 export async function createCashClosureAction(invoices: Invoice[], formData: FormData) {
   try {
     const branch = getBranch()
+
     const cashClosed = await cashClosuresRepository.create({
       branch,
       invoices,
       createdAt: new Date(),
       totalDaily: Number(formData.get('totalDaily')),
+      totalDailyCash: Number(formData.get('totalDailyCash')),
+      totalCash: Number(formData.get('totalCash')),
+      totalCard: Number(formData.get('totalCard')),
       dailyPercentage: Number(formData.get('dailyPercentage')),
       managerBonus: Number(formData.get('managerBonus')),
       employeeBonus: Number(formData.get('employeeBonus')),
       employeePayment: Number(formData.get('employeePayment')),
+      employeesNum: Number(formData.get('employeesNum')),
       totalCanceled: Number(formData.get('totalCanceled') ?? 0)
     })
     await invoiceRepository.setCashClosure({ invoices, cashClosure: cashClosed })
