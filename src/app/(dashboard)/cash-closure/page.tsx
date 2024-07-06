04c2eb765632e76cb8c080abc52c57f7a55db560
Franco de Desarrollo <francoegi01@gmail.com>
2024-07-06 17:00:51 -0300

    feat: Refactor cash closure page to calculate and display daily totals

--- src/app/(dashboard)/cash-closure/page.tsx
+++ src/app/(dashboard)/cash-closure/page.tsx
@@ -19,16 +19,49 @@ import { FormClashClosure } from './ui/FormClashClosure'
 
 export default async function Page() {
   const { invoices } = await getInvoicesToCashClosure()
-  const { total, cancelleds } = invoices.reduce(
+  // TODO: refactor this shit
+  const { totalDaily, cancelleds, totalCard, totalCash, totalDailyCash } = invoices.reduce(
     (acc, invoice) => {
       if (invoice.status === InvoiceStatusEnum.COMPLETED) {
-        acc.total += invoice.total
-      } else {
+        acc.totalDaily += invoice.total
+        const { totalCard, totalCash, totalDailyCash } = [
+          ...invoice.tickets,
+          ...invoice.items
+        ].reduce(
+          (acc, curr) => {
+            // Is ticket
+            if ('service' in curr) acc.totalDailyCash += curr.service?.cashPrice ?? 0
+            // Is product
+            if ('product' in curr) acc.totalDailyCash += curr.product.cashPrice ?? 0
+            if (curr.paymentMethod === 'cash') {
+              acc.totalCash += curr.totalPrice
+            } else {
+              acc.totalCard += curr.totalPrice
+            }
+            return acc
+          },
+          {
+            totalCard: 0,
+            totalDailyCash: 0,
+            totalCash: 0
+          }
+        )
+        acc.totalCard += totalCard
+        acc.totalCash += totalCash
+        acc.totalDailyCash += totalDailyCash
+      } else if (invoice.status === InvoiceStatusEnum.CANCELLED) {
         acc.cancelleds += invoice.total
       }
+
       return acc
     },
-    { total: 0, cancelleds: 0 }
+    {
+      totalDailyCash: 0,
+      totalCash: 0,
+      totalCard: 0,
+      totalDaily: 0,
+      cancelleds: 0
+    }
   )
 
   return (
@@ -65,12 +98,19 @@ export default async function Page() {
                 <TableCell className=' flex-1' colSpan={4}>
                   Total
                 </TableCell>
-                <TableCell>{currencyFormat(total)}</TableCell>
+                <TableCell>{currencyFormat(totalDaily)}</TableCell>
               </TableRow>
             </TableBody>
           </Table>
         </div>
-        <FormClashClosure invoices={invoices} total={total} cancelleds={cancelleds} />
+        <FormClashClosure
+          invoices={invoices}
+          totalDaily={totalDaily}
+          totalCard={totalCard}
+          totalCash={totalCash}
+          totalDailyCash={totalDailyCash}
+          cancelleds={cancelleds}
+        />
       </div>
     </ContainerPage>
   )
